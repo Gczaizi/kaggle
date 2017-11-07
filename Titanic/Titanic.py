a1ddd60bb36c0da694284f201d1c0959e98a0826
Zaizi <gc.zaizi@hotmail.com>
2017-11-07 10:01:11 +0800

    finished Data preprocessing

--- Titanic/Titanic.py
+++ Titanic/Titanic.py
@@ -139,4 +139,78 @@ for dataset in combine:
         for j in range(0, 3):
             dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j+1), 'Age'] = guess_ages[i,j]
     dataset['Age'] = dataset['Age'].astype(int)
-print(train_df.head())
+# print(train_df.head())
+
+
+train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+# print(train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
+
+
+for dataset in combine:
+    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[dataset['Age'] > 64, 'Age'] = 4
+# print(train_df.head())
+
+
+train_df = train_df.drop(['AgeBand'], axis=1)
+combine = [train_df, test_df]
+# print(train_df.head())
+
+
+for dataset in combine:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+# print(train_df[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+
+
+for dataset in combine:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+# print(train_df[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+
+
+train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+combine = [train_df, test_df]
+# print(train_df.head())
+
+
+for dataset in combine:
+    dataset['Age*Class'] = dataset.Age * dataset.Pclass
+# print(train_df.loc[:, ['Age*Class', 'Age', 'Pclass']].head())
+
+
+freq_port = train_df.Embarked.dropna().mode()[0]
+# print(freq_port)
+
+
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+# print(train_df[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+
+
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map({'S': 0 ,'C': 1, 'Q': 2}).astype(int)
+# print(train_df.head())
+
+
+test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
+# print(test_df.head())
+
+
+train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
+# print(train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True))
+
+
+for dataset in combine:
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31.0), 'Fare'] = 2
+    dataset.loc[dataset['Fare'] > 31.0, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+train_df = train_df.drop(['FareBand'], axis=1)
+combine = [train_df, test_df]
+# print(train_df.head(10))
+
